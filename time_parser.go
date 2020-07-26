fc823fc71a46009231fd620f7be6946759380a35
adri <aizpurua23a@gmail.com>
2020-07-26 21:40:41 +0200

    refactored

--- time_parser.go
+++ time_parser.go
@@ -9,6 +9,31 @@ import (
 	"strconv"
 )
 
+func main() {
+
+	file := openFile("test_input.txt")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+		line := scanner.Text()
+		match := timeRegExp.FindStringSubmatch(line)
+		result := make(map[string]string)
+		hour, min, meridiem := getResultsFromTimeParse(match, result)
+		DayMins := getMinutesOfDay(hour, min, meridiem)
+
+		fmt.Println("Time: ", line)
+		fmt.Println("Minutes past midnight: ", strconv.Itoa(DayMins))
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	closeFile(file)
+}
+
+var timeRegExp = regexp.MustCompile(`(?P<hours>\d\d?)(:(?P<minutes>\d\d))?(?P<meridiem>am|pm)?`)
+
 func openFile(filename string) (file *os.File) {
 	file, err := os.Open(filename)
 
@@ -22,8 +47,6 @@ func openFile(filename string) (file *os.File) {
 func closeFile(fp *os.File) {
 	fp.Close()
 }
-
-var timeRegExp = regexp.MustCompile(`(?P<hours>\d\d?)(:(?P<minutes>\d\d))?(?P<meridiem>am|pm)?`)
 
 func getMinutesOfDay(hour int, minutes int, meridiem string) (DayMins int) {
 	if meridiem == "am" && hour == 12 {
@@ -46,45 +69,32 @@ func getMinutesOfDay(hour int, minutes int, meridiem string) (DayMins int) {
 
 }
 
-func main() {
+func getResultsFromTimeParse(match []string, result map[string]string) (hour int, min int, meridiem string) {
 
-	file := openFile("test_input.txt")
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		match := timeRegExp.FindStringSubmatch(line)
-		result := make(map[string]string)
-		for i, name := range timeRegExp.SubexpNames() {
-			if i != 0 && name != "" {
-				result[name] = match[i]
-			}
+	for i, name := range timeRegExp.SubexpNames() {
+		if i != 0 && name != "" {
+			result[name] = match[i]
 		}
+	}
 
-		hour, err1 := strconv.Atoi(result["hours"])
+	hour, err1 := strconv.Atoi(result["hours"])
 
-		if err1 != nil {
+	if err1 != nil {
+		return
+	}
+
+	min = 0
+	var err2 error
+
+	if result["minutes"] != "" {
+		min, err2 = strconv.Atoi(result["minutes"])
+		if err2 != nil {
 			return
 		}
-
-		min := 0
-		var err2 error
-
-		if result["minutes"] != "" {
-			min, err2 = strconv.Atoi(result["minutes"])
-			if err2 != nil {
-				return
-			}
-		}
-
-		DayMins := getMinutesOfDay(hour, min, result["meridiem"])
-
-		fmt.Println("Time: ", line)
-		fmt.Println("Minutes past midnight: ", strconv.Itoa(DayMins))
-
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	closeFile(file)
+	meridiem = result["meridiem"]
+
+	return
+
 }
